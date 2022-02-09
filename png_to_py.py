039a0cf83553da455eb09c04f938b260d6b5e90f
Kwongwaiki <44832903+Kwongwaiki@users.noreply.github.com>
2022-02-10 02:19:12 +0800

    png to .py

---
+++ png_to_py.py
@@ -0,0 +1,16 @@
+import base64
+from PIL import Image
+
+file_png = r'icon.png'
+img = Image.open(file_png)
+img.save('icon.ico')
+
+with open('icon.ico','rb') as image_file:
+    encoded_string = base64.b64encode(image_file.read())
+
+ico_to_py = "img = ''%s'''" %encoded_string
+str(ico_to_py)
+ico_to_py = ico_to_py[0:8] + ico_to_py[9:-1]
+file = open('icon.py','w+')
+file.write(ico_to_py)
+file.close()
