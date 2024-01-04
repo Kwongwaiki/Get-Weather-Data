90a822cd9915aebc08e3d902686947fed2bf98f2
Kwongwaiki <44832903+Kwongwaiki@users.noreply.github.com>
2024-01-05 04:41:59 +0800

    Debug DailyExtract_HKO.py
    Daily Extract of Meteorological Observations [HKO]

--- DailyExtract_HKO.py
+++ DailyExtract_HKO.py
@@ -11,15 +11,20 @@ def getData(startYear, endYear):
         print('Progress: ' + year)
         xml = 'https://www.hko.gov.hk/cis/dailyExtract/dailyExtract_'+year+'.xml'
         response = requests.get(xml)
+        if response.status_code != 200:
+            break
         xml_content = response.text
-        json_content = json.loads(html_content)
+        json_content = json.loads(xml_content)
         for data in json_content['stn']['data']:
             month = str(data['month']).zfill(2)
             date = year+','+month+','
             for dayData in data['dayData']:
-                if not(dayData[0] in ['Mean/Total','Normal']):
+                if any(x in dayData[0] for x in ['Mean/Total','Normal']) != True:
                     dayNum = (datetime.strptime(year+month+dayData[0], "%Y%m%d")).strftime("%j")
-                    record = date+dayData[0]+','+dayNum+','+((",".join(dayData[1:])).replace(" ", "")).replace("Trace", "0.05")
+                    record = date+dayData[0]+','+dayNum+','+(",".join(dayData[1:])).replace("Trace", "0.05")
+                    removeChar = ['#', '^', '?', ' ', '*']
+                    for char in removeChar:
+                        record = record.replace(char, '')
                     recordList.append(record)
     return recordList
 
@@ -30,9 +35,9 @@ def dataTxt(outFile, dataList):
     
 if __name__ == '__main__':
     yearData = []
-    print("Inputs: [Year]")
-    startYear = int(input("Start from (min: 1884): ").strip() or datetime.today().year)
-    endYear = int(input("End in (max: current): ").strip() or datetime.today().year)
+    print("Inputs: [Year - by default >> current year]")
+    startYear = int(input("Start from (min >> 1884): ").strip() or datetime.today().year)
+    endYear = int(input("End in (max >> current year): ").strip() or datetime.today().year)
     if endYear > datetime.today().year:
         endYear = datetime.today().year + 1
     else:
@@ -41,5 +46,4 @@ if __name__ == '__main__':
     txtDir = str(input("TxT output file name (no extension): "))
     txtDir = str(Path().absolute()) + '/' + txtDir + '.txt'
     dataTxt(txtDir, yearData)
-    
     
